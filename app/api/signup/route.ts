26df9f083f0af1694ccba7c745b770efbc071d53
vidhyarth patel <vidyarthpatel@gmail.com>
2025-12-24 20:57:04 +0530

    fixes some deployment issues

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -9,6 +9,9 @@ const SignupSchema = z.object({
     password: z.string().min(8),
 });
 
+export const dynamic = "force-dynamic";
+
+
 export async function POST(req: Request) {
     try {
         const body = await req.json();
